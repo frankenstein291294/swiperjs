9f976aa4dabe09d7de506bed16ac28a402120f5b
frankenstein291294 <61632598+frankenstein291294@users.noreply.github.com>
2024-07-24 16:26:38 -0600

    Add files via upload

---
+++ swiper/main.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    loop: true,
+    // spaceBetween: 10,
+
+    pagination: {
+        type: 'progressbar',
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+/*     autoplay: { */
+        /* delay: 2000 */
+    /* }, */
+});
